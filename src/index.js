f871718ae46b2f06d4eda549101b6f181db1731f
Gagan-Aptagiri <gagan.aptagiri@gmail.com>
2021-05-22 17:11:22 +0530

    TODO App init

---
+++ src/index.js
@@ -0,0 +1,8 @@
+const app = document.getElementById('app');
+app.innerHTML = `
+  <div class="todos">
+    <div class="todos-header">
+      <h3 class="todos-title">Todo List</h3>
+    </div>
+  </div>
+`;

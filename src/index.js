223ffff8d0ebb1c8f4220ce7d1c7c6670287d912
Gagan-Aptagiri <gagan.aptagiri@gmail.com>
2021-05-22 17:48:14 +0530

    Adjusted the import for the Styles

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 const app = document.getElementById('app');
+
 app.innerHTML = `
   <div class="todos">
     <div class="todos-header">
       <h3 class="todos-title">Todo List</h3>
+      <div>
+        <p>You have <span class="todos-count"> </span> items</p>
+        <button class="todos-clear" style="display:none;">
+          Clear Completed
+        </button>
+      </div>
     </div>
+    <form class="todos-form" name="todos">
+      <input type="text" placeholder="What's next?" name="todo"/>
+    </form>
+    <ul class="todos-list"></ul>
   </div>
 `;
